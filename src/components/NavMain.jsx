f87b07e87f8c74813190a696eb418ba0aa78a6a6
Anstetten <h.zsola@gmail.com>
2021-09-10 18:20:04 +0200

    Creation of responsive navbar. + Burger button but dropdown menu not yet created

--- src/components/NavMain.jsx
+++ src/components/NavMain.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { withUser } from "../components/Auth/withUser";
 import apiHandler from "../api/apiHandler";
-
+import pineLogo from "../Images/pineapple.png"
 import "./NavMain.css";
+import {FaBars} from 'react-icons/fa'
 
 const NavMain = (props) => {
   const { context } = props;
+
+
 
   function handleLogout() {
     apiHandler
@@ -21,18 +24,27 @@ const NavMain = (props) => {
 
   return (
     <nav className="NavMain">
-      <NavLink exact to="/">
-        <h3 className="logo">App name</h3>
+      <NavLink className="logoWrapper" exact to="/">
+        <img id="smallLogo" src={pineLogo} alt="logo"/>
+        <h3 className="logo">PineaPlanner</h3>
       </NavLink>
+      <FaBars className='bars'/>
       <ul className="nav-list">
+        <div className="navLeft">
           <li>
-            <NavLink to="/recipes">Recipes</NavLink>
+            <NavLink  to="/recipes">Recipes</NavLink>
           </li>
+          {context.isLoggedIn &&(
+                          <li>
+                          <NavLink  to="/planner">Meal Planner</NavLink>
+                          </li>
+          ) }
+        </div>
+
         {context.isLoggedIn && (
           <React.Fragment>
-              <li>
-            <NavLink to="/planner">Meal Planner</NavLink>
-            </li>
+
+            <div className="navRight">
             <li>
               <NavLink to="/profile">
                 {context.user && context.user.email}
@@ -41,16 +53,19 @@ const NavMain = (props) => {
             <li>
               <p onClick={handleLogout}>Logout</p>
             </li>
+            </div>
           </React.Fragment>
         )}
         {!context.isLoggedIn && (
           <React.Fragment>
+            <div className="navRight">
             <li>
               <NavLink to="/signin">Log in</NavLink>
             </li>
             <li>
               <NavLink to="/signup">Create account</NavLink>
             </li>
+            </div>
           </React.Fragment>
         )}
       </ul>
